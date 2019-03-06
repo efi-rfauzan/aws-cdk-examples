cdf85e1b77da8f09064193f6debc0c0b8454df16
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-03-06 01:57:23 -0800

    Fix ECS examples (#14)
    * Fix cluster example
    
    * Fix ecs service with advanced alb config
    
    * Fix load balanced service
    
    * Update packages in fargate load balanced service
    
    * Fix service w/ task placement

--- typescript/ecs/cluster/index.ts
+++ typescript/ecs/cluster/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a4c224577a9fdf277c25948c3f13bcfe277414bf15d83e19c2dc0bea4c9b895
-size 942
+oid sha256:99c5b1bf81d3f786cd34398fdd601bb10c65e65272e7c17402261a5317d4ced3
+size 1055
