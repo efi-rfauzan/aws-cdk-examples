063a0b3949b3e92a78dcb0a7ac1c85c14f897b9d
Rico Huijbers <rix0rrr@gmail.com>
2019-04-25 14:52:04 +0200

    Update examples to 0.29.0 (#35)
    - Automatically test Python examples
    - Remove chat-app examples

--- python/ecs/cluster/app.py
+++ python/ecs/cluster/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45c5b7440441aaae757832b26459da92e54dc5015a4764f30a65c689e1dd6902
-size 1033
+oid sha256:ddf6c8d0dafef66112b56c4daf55e5a174dccf079baf45ae493f87cd40277107
+size 1097
