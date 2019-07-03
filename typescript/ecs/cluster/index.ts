b8854fef2a88fc2f1bb855b1c894218e2eed9d58
tbadlov <zvika@badalov.us>
2019-07-03 15:12:59 -0400

    fix: many updates to fix for CDK 0.36.0 (#62)
    * Change @aws-cdk/cdk to @aws-cdk/core
    * change in typescript files as well
    * changed values for resoruce overrides
    * more updates to latest apis
    * more updates
    * added commet
    * more cleanup
    * updates to more files
    * static site patch
    * fix time
    * Update CONTRIBUTING.md - Update documentation to align version `0.36.0`
    
    Co-Authored-By: Tom Dufall <tom@tomdufall.co.uk>
    * fix instance type constructor to include a combincation of class and size
    * fix path resolution for fromAsset
    * nit cleanup
    * better definition of instane type
    * timeout to 300 seconds
    * change from AssetCode to Code
    * added cluster capacity for further investigation
    * added optional properities back
    * extra space
    * uncomment code for further investigation
    * added timeout duration

--- typescript/ecs/cluster/index.ts
+++ typescript/ecs/cluster/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c91985e596fccc60e56e37cbaf47401a55297471f7f0e5795ec2f17ae4c9602
-size 1013
+oid sha256:9c1b870c5422554bf3c3a642702fe0f43033eb30c050447eab6a91c5a5d810de
+size 1430
