e4a5cb9d95444fa23021d1594d752f9ad4798b4f
Adam Ruka <adamruka@amazon.com>
2019-11-22 11:47:38 -0800

    fix: update the Python URL shortener example with the new cdk.context.json used by the VPC provider (#170)
    Fixes the build of the project.

--- python/ecs/cluster/app.py
+++ python/ecs/cluster/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77375b2fdc44e5418296a2eff7b82294bae7c469bd4c1c5c98a02771234cb06f
-size 1096
+oid sha256:181548c0f12c8923dfea1ee176f46708b6f0193f74ca53ecbeb673c1bf284e30
+size 1100
