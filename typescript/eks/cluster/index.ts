1bb8d60c819c0dd72e242bef7866f9983c1b32e9
Niranjan Jayakar <nija@amazon.com>
2020-07-10 13:27:09 +0100

    fix(typescript/eks): compile fails for missing property 'version' (#315)
    This is due to the breaking change in the eks module introduced in
    version 1.50.0.
    https://github.com/aws/aws-cdk/blob/master/CHANGELOG.md#1500-2020-07-07

--- typescript/eks/cluster/index.ts
+++ typescript/eks/cluster/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dfdabd5a7c445195a98a3944b9251d3207732833d3e52c9e128116cde4dbdd4
-size 1472
+oid sha256:35106928fe0d7a91b8b9f7667efb82de5e707d821d8e9b7547bd52d05d2831f8
+size 1517
