38dd05e4319875c64a040fabbaf49b434be23765
Rico Huijbers <rix0rrr@gmail.com>
2020-12-23 17:46:10 +0100

    fix: `kubeCtlEnabled` is no longer necessary (#375)

--- typescript/eks/cluster/index.ts
+++ typescript/eks/cluster/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47232625720a12d8cda57a16e0a4ed99b50b602d6adc7ddf0979f85df6064ced
-size 1529
+oid sha256:88c5840c1b9e9349ce42bf449c8851f850e06f0b642dc95173964c9f0050433a
+size 1444
