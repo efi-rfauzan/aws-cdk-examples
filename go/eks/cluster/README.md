369d617beb6400bed0a667f63957a26931768b2c
Eli Polonsky <epolon@amazon.com>
2021-04-01 15:04:32 +0300

    feat(go): EKS Cluster (#405)
    * first attempt
    
    * Use empty struct instead of nil
    
    * restructure using the init template

---
+++ go/eks/cluster/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a40f098c4a64a23c9bd7102f79d6186ece27aad965faf3857a5f7d694b820553
+size 873
---
+++ go/eks/cluster/cluster_test.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a7a28c4dd3363860e8a06d867969691a7fe43f1c14feee0ecfdadb205605e1f
+size 588
