b24dc5b39bb979e2ded8bce802a2aa750b8764b9
Sam Wilson <samtwil@live.com>
2022-05-11 17:36:00 -0500

    fix(python): Updated type checking for Python samples (#554)
    * fix(python): Updated ecs cluster ASG vpc_subnets parameter. Fixed build issue with image-content-search
    
    * fix(python): Resolved type checks for api-cors-lambda example
    
    * feat(python): Resolved type checks for waf example
    
    * fix(python): Resolved type checks for url-shortener example
    
    * fix(python): Resolved type checks for stepfunctions example
    
    * fix(python): Resolved type checks for rekognition-lambda-s3-trigger example
    
    * fix(python): Resolved type checks for new-vpc-alb-asg-mysql example
    
    * fix(python): Resolved type checks for my-widget-service example
    
    * fix(python): Resolved type checks for lambda-from-container example
    
    * fix(python): Resolved type checks for lambda-ddb-mysql-etl-pipeline example
    
    * fix(python): Resolved type checks for lambda-cloudwatch-dashboard example
    
    * fix(python): Resolved type checks for image-content-search example
    
    * fix(python): Resolved type checks for existing-vpc-new-3c2-ebs-userdata example
    
    * fix(python): Resolved type checks for api-eventbridge-lambda example
    
    * Fixed mypy errors
    
    * Further mypy fixes before official code review
    
    * Adding types to requirements.txt instead of ignoring types. Minor code comparison cleanup
    
    * Minor import cleanup
    
    * fix: resolving initial code review comments for python stacks
    
    Co-authored-by: Sam Wilson <zsamwil@amazon.com>
    Co-authored-by: Chinedu Obiakara <obiakarachinedu@gmail.com>

--- python/ecs/cluster/app.py
+++ python/ecs/cluster/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd94d41b10932d1340125bc229fbc33c74f1f472442b721f09dcae98e117be4
-size 1087
+oid sha256:e67325e15d2e664f4400d4c7c1a7332384458ade37d8204503bd1956b04c2f5c
+size 1102
