b92870a9d435ee0bd468a84a89fb3f0d627771cc
Mirza Bilal <bilal@mirzabilal.com>
2023-10-25 19:42:24 +0500

    Fix(ts:ecs/cluster):Updated Instance class to X86_64 3rd Gen Burstable (#942)
    * Upgraded ECS Cluster Example to 4th Gen Burstable.
    Updated ECS Cluster Machine Image to AL2023
    
    * Updated Instance Class to 3rd Gen in ECS NLB
    
    * Updated ECS instance class to Burstable 3rd Gen
    
    * Updated ECS Task Placment instance to 3rd Gen AMD
    
    ---------
    
    Co-authored-by: Michael Kaiser <mjkaiser@gmail.com>

--- typescript/ecs/cluster/index.ts
+++ typescript/ecs/cluster/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:072e153f8dbf6f5a320d05f6abb0a08aed04c117948abd2230b75b2be02bca31
-size 969
+oid sha256:23b8c983754a9a03217986025685c6f64794e47600ae8c1d27ed4360db1fecf1
+size 1046
